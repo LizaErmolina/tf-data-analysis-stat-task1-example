6c471278aa19e1b3d72959d170e3c00151f03af6
LizaErmolina <129836554+LizaErmolina@users.noreply.github.com>
2023-04-06 23:44:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 33259271 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    
-    return x.mean() # Ваш ответ
+      n = len(x)
+    print(n)
+    t = 46
+    lam = x.mean()/t
+    return lam # Ваш ответ
